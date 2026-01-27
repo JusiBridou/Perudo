6d111031fbc3917eb3eb29d826550e7b3795ce51
JusiBridou <juslamarcade@gmail.com>
2026-01-27 17:21:32 +0100

    Page introuvable :/

--- perudo-app/backend/src/main/java/com/perudo/api/SpaController.java
+++ perudo-app/backend/src/main/java/com/perudo/api/SpaController.java
@@ -2,10 +2,16 @@ package com.perudo.api;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController implements ErrorController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
 
     @RequestMapping("/error")
     public String handleError() {
